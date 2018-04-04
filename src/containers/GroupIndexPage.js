b4b13b23f52abb06a69a831da8601b7e6ad57b3e
nsch1 <nick.schooneman1@gmail.com>
2018-04-04 13:56:33 +0200

    Add button to add groups

--- src/containers/GroupIndexPage.js
+++ src/containers/GroupIndexPage.js
@@ -2,6 +2,8 @@ import React, {PureComponent} from 'react'
 import Row from "antd/es/grid/row"
 import Card from "antd/es/card/index"
 import GroupList from "../components/GroupList"
+import Button from "antd/es/button/button"
+import Col from "antd/es/grid/col"
 
 export default class GroupIndex extends PureComponent {
 
@@ -10,6 +12,11 @@ export default class GroupIndex extends PureComponent {
       <Row type="flex" justify="space-around" align="middle">
         <Card title="Classes" bordered={false} style={{maxWidth: '700px', width: '70vw'}}>
           <GroupList/>
+          <Button
+            type="primary"
+          >
+            Add Class
+          </Button>
         </Card>
       </Row>
     )
