cf641f981d8fb6dc47d18ff05a706fac62f441e8
nsch1 <nick.schooneman1@gmail.com>
2018-04-04 17:26:41 +0200

    Add form state

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -5,16 +5,29 @@ import Button from "antd/es/button/button"
 const {Item} = Form
 
 export default class LoginForm extends PureComponent {
-  //state = {}
+  state = {}
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(this.state)
+  }
+
+  handleChange = (e) => {
+    const {name, value} = e.target
+
+    this.setState({
+      [name]: value
+    })
+  }
 
   render() {
     return (
-      <Form layout="inline">
+      <Form layout="inline" onSubmit={this.handleSubmit}>
         <Item>
-          <Input type="email" placeholder="Email" />
+          <Input name="email" type="email" placeholder="Email" onChange={this.handleChange} />
         </Item>
         <Item>
-          <Input type="password" placeholder="Password" />
+          <Input name="password" type="password" placeholder="Password" onChange={this.handleChange} />
         </Item>
         <Item>
           <Button
